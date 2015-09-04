404e28a9add74272bdd61c96065135f869331ccf
fungi-fungi <psyfungus@gmail.com>
2015-09-04 16:00:08 +0300

    Initial commit

---
+++ platforms/android/assets/www/js/app.js
@@ -0,0 +1,13 @@
+angular.module('TimeMachine', ['ngRoute', 'TimeMachine.services', 'TimeMachine.controllers'])
+    .config(function ($routeProvider) {
+        $routeProvider
+        .when('/', {
+            controller: 'MainCtrl',
+            templateUrl: 'partials/main.html'
+        })
+        .when('/view', {
+            controller: 'ViewCtrl',
+            templateUrl: 'partials/view.html'
+        })
+        .otherwise({redirectTo: '/'});
+    });
