37f28e62caa5df23ac89d1aff0cfca75bca840ef
fungi-fungi <psyfungus@gmail.com>
2015-10-04 22:19:58 +0300

    Mark fields red in login page

--- www/scripts/controllers/login.js
+++ www/scripts/controllers/login.js
@@ -8,17 +8,22 @@ angular.module('gwTimeMachine')
       apiService.login(user).then(
         function(res){
           console.log(res);
-          if (res.data.data.authentication_token && res.data.data.authentication_token){
+          if (res.data.data.authentication_token){
             $localStorage.user = res.data;
             $location.path('/main');
           }else{
-            $scope.loginForm.$invalid = true;
+            setFormInvalid();
           }
         },
         function(err){
-          $scope.loginForm.$invalid = true;
+          setFormInvalid();
         }
       );
     };
+
+    function setFormInvalid(){
+      $scope.loginForm.email.$invalid = true;
+      $scope.loginForm.password.$invalid = true;
+    }
 
 });
